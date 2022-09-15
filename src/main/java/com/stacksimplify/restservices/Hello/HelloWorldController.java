88c901fdc291dfcc7a2df81ad6099e352a792b8b
sam <sher_singh1@yahoo.com>
2022-09-15 00:30:22 -0400

    Adding changes made as we progress

--- src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.stacksimplify.restservices.Hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,4 +12,14 @@ public class HelloWorldController {
 	public String helloWorld() {
 		return "Hello World!";
 	}
+	
+	@GetMapping("/helloWorld1")
+	public String helloWorld1() {
+		return "Hello World1!";
+	}
+	
+	@GetMapping("/helloWorld-bean")
+	public UserDetails helloWorldBean() {
+		return new UserDetails("Sam", "Bahadur", "Nepal");
+	}	
 }
