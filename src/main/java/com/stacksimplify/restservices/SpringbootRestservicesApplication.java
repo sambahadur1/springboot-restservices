52cbd2a0b2b50ba68cf13f03ec72269a1bb0d0f5
sam <sher_singh1@yahoo.com>
2022-09-14 22:24:05 -0400

    Initial commit

---
+++ src/main/java/com/stacksimplify/restservices/SpringbootRestservicesApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestservicesApplication.class, args);
+	}
+
+}
